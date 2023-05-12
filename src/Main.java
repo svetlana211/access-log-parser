a4c278db3dc7061b7e4b9e4a17569cb2e9e815ed
Svetlana Kapitonova <s.kapitonova@bk.ru>
2023-05-12 17:37:13 +0300

    new logic with numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите число 1");
+        int x = new Scanner(System.in).nextInt();
+        System.out.println("Введите число 2");
+        int y = new Scanner(System.in).nextInt();
+        System.out.println("Сумма двух чисел : "+(x+y));
+        System.out.println("Разность двух чисел : "+(x-y));
+        System.out.println("Произведение двух чисел : "+(x*y));
+        System.out.println("Частное двух чисел : "+(double)(x/y));
     }
 }
