08f8521c586253cee3ed88bc25a3136aaa940ec3
Svetlana Kapitonova <s.kapitonova@bk.ru>
2023-05-25 16:07:31 +0300

    Path finder added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        isFile();
+    }
+    public static void isFile() {
+        int countFiles=0;
+    for (int i=0; ;i++) {
+        String path = new Scanner(System.in).nextLine();
+        File file = new File(path);
+        if (file.isDirectory()) {
+            System.out.println("Указан путь к папке, а не файлу"); continue;
+        } else if (!file.exists()) {
+            System.out.println("Файл не существует"); continue;
+        } else if (file.exists() && !file.isDirectory()) {
+            System.out.println("Путь указан верно. Это файл номер "+ ++countFiles);
+        }
+        }
     }
 }
